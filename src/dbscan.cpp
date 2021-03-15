43b99ddc0cc7c7183356fc906f2e8f185b65eee4
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-15 15:49:58 +0100

    flexible interface for different dbscan approaches

--- src/dbscan.cpp
+++ src/dbscan.cpp
@@ -1,64 +1,25 @@
 #include "dbscan.h"
 #include "logger.h"
+#include "modified.h"
+#include "original.h"
 #include "timer.h"
 
-std::vector<Point> query(
-    const std::vector<Point>& points, Point core, const float& epsilon)
+std::pair<std::vector<Point>, int> dbscan::run(
+    std::vector<Point>& points, const int& minPoints, const float& epsilon)
 {
-    /** neighboring points */
-    std::vector<Point> neighbours;
-    for (const auto& point : points) {
-        if (core.distance(point) <= 90) {
-            neighbours.push_back(point);
-        }
+    std::pair<std::vector<Point>, int> clustered;
+    // {
+    //     Timer timer;
+    //     clustered = original::cluster(points, minPoints, epsilon);
+    //     LOG(INFO) << timer.getDuration()
+    //               << " ms: dbscan runtime (!unoptimized!) ";
+    // }
+    {
+        Timer timer;
+        modified dbscan(points);
+        clustered = {dbscan.m_points, dbscan.m_clusterCount};
+        LOG(INFO) << timer.getDuration()
+                  << " ms: dbscan runtime (!unoptimized!) ";
     }
-    return neighbours;
-}
-
-std::vector<Point> DBSCAN::cluster(std::vector<Point> points, const int& minPoints, const float& epsilon)
-{
-    Timer timer;
-/**
- * DBSCAN
- *   Original query-based algorithm:
- *   https://en.wikipedia.org/wiki/DBSCAN
- *   date: 2021-03-06 16:04
- */
-    int cluster = -1;
-    for (auto& point : points) {
-        if (point.m_cluster != UNCLASSIFIED) {
-            continue;
-        }
-
-        /** neighboring points */
-        std::vector<Point> neighbours = query(points, point, 90);
-        if (neighbours.size() < minPoints) {
-            point.m_cluster = NOISE;
-            continue;
-        }
-        cluster++;
-        point.m_cluster = cluster;
-        // seedset missing here !!!
-
-        for (auto& n : neighbours) {
-            if (n.m_cluster == NOISE) {
-                n.m_cluster = cluster;
-            }
-            if (n.m_cluster != UNCLASSIFIED) {
-                continue;
-            }
-            n.m_cluster = cluster;
-
-            /** nearest neighbors */
-            std::vector<Point> nn = query(points, n, 90);
-            if (nn.size() >= minPoints) {
-                neighbours.insert(neighbours.end(), nn.begin(), nn.end());
-            }
-        }
-    }
-    LOG(INFO) << timer.getDuration() << " ms: dbscan runtime (!unoptimized!) ";
-
-    // io::write(points, io::pwd() + "/build/dbscan.csv");
-
-    return points;
+    return clustered;
 }
