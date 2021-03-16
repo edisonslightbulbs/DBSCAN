d9faa64df97f3e532ed0b452b54e7978f9536a52
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-16 10:58:19 +0100

    cleaning code base

--- src/dbscan.cpp
+++ src/dbscan.cpp
@@ -1,25 +1,19 @@
+#include <memory>
+
 #include "dbscan.h"
 #include "logger.h"
-#include "optimized.h"
 #include "original.h"
 #include "timer.h"
 
-std::pair<std::vector<Point>, int> dbscan::run(
+std::pair<std::vector<Point>, int> dbscan::cluster(
     std::vector<Point>& points, const int& minPoints, const float& epsilon)
 {
+    Timer timer;
+
+    auto sptr_points = std::make_shared<std::vector<Point>>(points);
+
     std::pair<std::vector<Point>, int> clustered;
-    {
-        Timer timer;
-        clustered = original::cluster(points, minPoints, epsilon);
-        LOG(INFO) << timer.getDuration()
-                  << " ms: dbscan runtime (!unoptimized!) ";
-    }
-    // {
-    //     Timer timer;
-    //     optimized dbscan(points);
-    //     clustered = {dbscan.m_points, dbscan.m_clusterCount};
-    //     LOG(INFO) << timer.getDuration()
-    //               << " ms: dbscan runtime (!unoptimized!) ";
-    // }
+    clustered = original::run(sptr_points, minPoints, epsilon);
+    LOG(INFO) << timer.getDuration() << " ms: dbscan runtime (!unoptimized!)";
     return clustered;
 }
