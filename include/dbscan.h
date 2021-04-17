045ddd0f5263ed9ad5699dc43f3913b22289a984
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-17 19:47:45 +0200

    bad name? using original to identify implementation of the original dbscan algorithm

--- include/dbscan.h
+++ include/dbscan.h
@@ -5,6 +5,6 @@
 #include <vector>
 
 namespace dbscan {
-std::pair<std::vector<Point>, int> run(std::vector<Point>& points);
+std::pair<std::vector<Point>, int> original(std::vector<Point>& points);
 }
 #endif /* DBSCAN_H */
