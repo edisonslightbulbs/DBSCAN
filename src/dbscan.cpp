d4a3a91349570c98c32bc41be505cdd6186bac58
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-21 15:17:23 +0200

    checkpoint (initial implementation of segment context rendering)

--- src/dbscan.cpp
+++ src/dbscan.cpp
@@ -1,30 +1,26 @@
 #include <vector>
 
 #include "dbscan.h"
+#include "kdtree.h"
 #include "original.h"
 #include "point.h"
-#include "kdtree.h"
 
-std::pair<std::vector<Point>, int> dbscan::cluster(std::vector<Point>& points)
+std::pair<std::vector<Point>, int> dbscan::cluster(
+    std::vector<Point>& points, const float& E, const int& N)
 {
-    /** dbscan parameters */
-    float epsilon = 3.317;
-    const int MIN_POINTS = 4;
-
     /** implementation 1) dbscan using original algorithm */
     auto sptr_points = std::make_shared<std::vector<Point>>(points);
-
-    // int numClusters = original::cluster(sptr_points, MIN_POINTS, epsilon); // <-naive
-    // naive implementation: original implementation of dbscan
+    // int numClusters = original::cluster(sptr_points, MIN_POINTS, epsilon); //
+    // <-naive naive implementation: original implementation of dbscan
     // translation of algorithm provided on wiki:
     // see@: https://en.wikipedia.org/wiki/DBSCAN
     //
 
     /** implementation 2) dbscan using kdtree (nanoflann's kdtree) */
-    int numClusters = kdtree::cluster(sptr_points, MIN_POINTS, epsilon); // <-optimized
-    // optimized implementation: based on nanoflann's optimized kdtree implementations
-    // see@: https://github.com/jlblancoc/nanoflann
+    int numClusters = kdtree::cluster(sptr_points, N, E); // <-optimized
+    // optimized implementation: based on nanoflann's optimized kdtree
+    // implementations see@: https://github.com/jlblancoc/nanoflann
     //
 
-    return {*sptr_points, numClusters };
+    return { *sptr_points, numClusters };
 }
